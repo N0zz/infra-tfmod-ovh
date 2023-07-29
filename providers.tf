713ee80f2ededee9234df324654cb88245ff63e7
Krzysztof Kolodziejczyk <kkolodziejczyk@gog.com>
2023-07-29 15:44:55 +0200

    change from tfvars to env vars

--- providers.tf
+++ providers.tf
@@ -1,6 +1,1 @@
-provider "ovh" {
-  endpoint           = var.ovh_endpoint
-  application_key    = var.ovh_application_key
-  application_secret = var.ovh_application_secret
-  consumer_key       = var.ovh_consumer_key
-}
+provider "ovh" {}
--- variables.tf
+++ variables.tf
@@ -1,21 +1,3 @@
-variable "ovh_endpoint" {
-    description = "Defined in Terraform Cloud variables set."
-    type = string
-    default = "ovh-eu"
-}
-variable "ovh_application_key" {
-    description = "Defined in Terraform Cloud variables set."
-    type = string
-}
-variable "ovh_application_secret" {
-    description = "Defined in Terraform Cloud variables set."
-    type = string
-}
-variable "ovh_consumer_key" {
-    description = "Defined in Terraform Cloud variables set."
-    type = string
-}
-
 variable "dns_records" {
     description = "Define dns records set in OVH DNS zones."
     type = map
